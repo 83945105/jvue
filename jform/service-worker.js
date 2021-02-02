22e1cbd7c0865e32f5212da51caddd54fffd4c41
18606167809@163.com <18606167809@163.com>
2021-02-02 09:48:19 +0800

    部署测试

--- jform/service-worker.js
+++ jform/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.2f683b2041d763b14f6c6cee0118e86c.js"
+  "precache-manifest.68f4338c13cec5bccfeb3fbeee864c0f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue3.0"});
