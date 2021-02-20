46dc667d3dc4ad453e9b30f836c21b97ef7a4776
18606167809@163.com <18606167809@163.com>
2021-02-20 22:28:31 +0800

    @jvue/jform 发布 1.0.16-test 版本

--- jform-ui/src/utils/deep-merge.js
+++ jform-ui/src/utils/deep-merge.js
@@ -10,5 +10,20 @@ const merge = require('deepmerge');
  * @returns {*}
  */
 export default function deepMerge() {
-    return merge.all([...arguments].filter(arg => arg));
+  return merge.all([...arguments].filter(arg => arg), {
+    // 自定义数组合并逻辑, 数组相同下标元素继续合并
+    arrayMerge: (target, source, options) => {
+      const destination = target.slice();
+      source.forEach((item, index) => {
+        if (typeof destination[index] === 'undefined') {
+          destination[index] = options.cloneUnlessOtherwiseSpecified(item, options)
+        } else if (options.isMergeableObject(item)) {
+          destination[index] = merge(target[index], item, options)
+        } else if (target.indexOf(item) === -1) {
+          destination.push(item)
+        }
+      });
+      return destination;
+    }
+  });
 };
